8b8bc3f2dc109469f0913c435cc5678ac2dc9245
EC2 Default User <ec2-user@ip-172-31-22-136.ec2.internal>
2019-03-21 23:58:54 +0000

    First Commit

---
+++ microservice-demo-order/src/main/java/com/ewolff/microservice/order/logic/OrderController.java
@@ -0,0 +1,93 @@
+package com.ewolff.microservice.order.logic;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ewolff.microservice.order.clients.CatalogClient;
+import com.ewolff.microservice.order.clients.Customer;
+import com.ewolff.microservice.order.clients.CustomerClient;
+import com.ewolff.microservice.order.clients.Item;
+
+@Controller
+class OrderController {
+
+	private OrderRepository orderRepository;
+
+	private OrderService orderService;
+
+	private CustomerClient customerClient;
+	private CatalogClient catalogClient;
+
+	@Autowired
+	private OrderController(OrderService orderService,
+			OrderRepository orderRepository, CustomerClient customerClient,
+			CatalogClient catalogClient) {
+		super();
+		this.orderRepository = orderRepository;
+		this.customerClient = customerClient;
+		this.catalogClient = catalogClient;
+		this.orderService = orderService;
+	}
+
+	@ModelAttribute("items")
+	public Collection<Item> items() {
+		return catalogClient.findAll();
+	}
+
+	@ModelAttribute("customers")
+	public Collection<Customer> customers() {
+		Collection<Customer> allCustomers = customerClient.findAll();
+//		Collection<Customer> newCustomer = new Collection<Customer>();
+		Iterator<Customer> itr = allCustomers.iterator();
+		while (itr.hasNext()) {
+			Customer cust = itr.next();
+			long id = cust.getCustomerId();
+			customerClient.getOne(id);
+		}
+		return allCustomers;
+	}
+
+	@RequestMapping("/")
+	public ModelAndView orderList() {
+		return new ModelAndView("orderlist", "orders",
+				orderRepository.findAll());
+	}
+
+	@RequestMapping(value = "/form.html", method = RequestMethod.GET)
+	public ModelAndView form() {
+		return new ModelAndView("orderForm", "order", new Order());
+	}
+
+	@RequestMapping(value = "/line", method = RequestMethod.POST)
+	public ModelAndView addLine(Order order) {
+		order.addLine(0, catalogClient.findAll().iterator().next().getItemId());
+		return new ModelAndView("orderForm", "order", order);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ModelAndView get(@PathVariable("id") long id) {
+		return new ModelAndView("order", "order", orderRepository.findById(id).get());
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ModelAndView post(Order order) {
+		order = orderService.order(order);
+		return new ModelAndView("success");
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ModelAndView post(@PathVariable("id") long id) {
+		orderRepository.deleteById(id);
+
+		return new ModelAndView("success");
+	}
+
+}
